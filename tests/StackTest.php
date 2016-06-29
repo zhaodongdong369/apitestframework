840af9e662c667818645bf6b517d5bc120733ea3
zhaodongdong <zhaodongdong@meituan.com>
2016-06-29 16:24:12 +0800

    fix

--- tests/StackTest.php
+++ tests/StackTest.php
@@ -4,7 +4,7 @@ class StackTest extends PHPUnit_Framework_TestCase
     public function testPushAndPop()
     {
         $stack = array();
-        $this->assertEquals(0, count($stack));
+        $this->assertEquals(1, count($stack));
     }
 }
 ?>
